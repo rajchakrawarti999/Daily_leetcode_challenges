69822bc0e55d95ae4f42f32a3bf8929d81a415b3
Raj Chakrawarti <rajchakrawarti787@gmail.com>
2026-06-28 23:15:31 +0530

    Time: 0 ms (100%), Space: 28.1 MB (65.07%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,22 +1,7 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int n = nums.size();
-
-        unordered_map<int, int> mp;
-        for(auto x : nums){
-            mp[x]++;
-        }
-        
-        int count = 0;
-        int ans;
-        for( auto x : mp){
-            if(x.second > count){
-                ans = x.first;
-                count = x.second;
-            }
-        }
-        return ans;
-        
+        sort(nums.begin(),nums.end());
+        return nums[(nums.size())/2];
     }
 };

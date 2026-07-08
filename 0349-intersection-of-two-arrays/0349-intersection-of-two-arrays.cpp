696f7679d31949a2b262619a2d15c565d056d1e3
Raj Chakrawarti <rajchakrawarti787@gmail.com>
2026-07-08 06:58:53 +0530

    Time: 0 ms (100%), Space: 13.7 MB (98.48%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -2,40 +2,48 @@ class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
         
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+
+        int i = 0;
+        int j =0;
         vector<int> ans;
-        
+        while(i < nums1.size() &&  j < nums2.size()){
 
-        for(int i=0; i<nums1.size(); i++){
+            if(nums1[i] > nums2[j]){
+                j++;
+            }
+            else if (nums1[i] < nums2[j]){
+                i++;
+            }
+            else{
 
-            int j = 0;
-            for(int j = 0; j<nums2.size(); j++){
-                if(nums1[i]==nums2[j]){
-                    
-                    if(ans.size()==0){
-                        ans.push_back(nums2[j]);
-                        break;
-                    }
-                    
-
+                if(ans.size()==0){
+                    ans.push_back(nums1[i]);
+                    i++;
+                    j++;
+                    continue;
+                }
+                else{
                     int k = 0;
-                   bool flag = false;
-                    while(k < ans.size()){
-                        if(ans[k]==nums2[j]){
-                            flag = true;
-                           break;
+                    bool flag = true;
+                    while(k< ans.size()){
+                        if(nums2[j]==ans[k]){
+                            flag = false;
+                            break;
                         }
                         k++;
                     }
-                    if(flag == false){
-                        ans.push_back(nums2[j]);
+                    if(flag==true){
+                        ans.push_back(nums1[i]);
                     }
-                    
-                
+                    i++;
+                    j++;
                 }
             }
-
+              
+            
         }
         return ans;
-        
     }
 };

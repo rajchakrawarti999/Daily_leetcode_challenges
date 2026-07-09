a53c7149d86f94b0e0200422b2f2fe84633284d7
Raj Chakrawarti <rajchakrawarti787@gmail.com>
2026-07-09 19:14:40 +0530

    Time: 0 ms (100%), Space: 8.3 MB (98.23%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public:
     int reverse(int x) {
-        long long  r=0;
-    while(x!=0){
-        long long ld =x%10; //id=last digit
-        r = r*10;
-        r = r+ld;
-        x/=10;
-        if (r > INT_MAX || r < INT_MIN)  // overflow check
-                return 0;
-    }
-    return (int)r;
+        int n = x;
+        long long  ans = 0;
+
+        while(n!=0){
+
+            long long  mod = n%10;
+            ans = ans * 10 + mod;
+            n /= 10;
+        }
+        
+        if(ans > INT_MAX || ans < INT_MIN){
+            return 0;
+        }
+
+        return (int) ans;
     }
 };

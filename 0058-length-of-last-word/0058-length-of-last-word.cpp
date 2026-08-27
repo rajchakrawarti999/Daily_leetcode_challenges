d014a101a613d5455a6d314a7b7eb828dc0b1186
Raj Chakrawarti <rajchakrawarti787@gmail.com>
2026-08-27 21:57:54 +0530

    Time: 0 ms (100%), Space: 8.8 MB (92.63%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        int n = s.size();
+        int  n = s.size();
         int count = 0;
+
         for(int i=n-1; i>=0; i--){
+
             if(count!=0 && s[i]==' ') break;
             if(s[i]==' ') continue;
-            if(s[i]!=' ') count++;
-            
-            
+            else{
+                count++;
+            }
         }
         return count;
     }
